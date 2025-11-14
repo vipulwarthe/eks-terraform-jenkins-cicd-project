90e50a18cd96a5aeb4b3608bbb6b24136638c4c9
Vipulkumar Warthe <126955342+vipulwarthe@users.noreply.github.com>
2025-11-14 16:25:39 +0530

    Update output.tf

--- Jenkins_server/tf-aws-ec2/output.tf
+++ Jenkins_server/tf-aws-ec2/output.tf
@@ -2,3 +2,8 @@ output "ec2_instance_ip" {
   description = "Public IP address of the EC2 instance"
   value       = module.ec2_instance.public_ip
 }
+
+output "ec2_instance_dns" {
+description = "Public DNS of the EC2 instance"
+value = module.ec2_instance.public_dns
+}
