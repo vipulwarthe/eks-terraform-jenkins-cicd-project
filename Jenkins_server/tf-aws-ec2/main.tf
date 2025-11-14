2f4a343a1b2673bfdce656580a9f17fd0982220f
Vipulkumar Warthe <126955342+vipulwarthe@users.noreply.github.com>
2025-11-14 15:59:41 +0530

    Create main.tf

---
+++ Jenkins_server/tf-aws-ec2/main.tf
@@ -0,0 +1,114 @@
+############################################
+# VPC MODULE
+############################################
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnets
+
+  map_public_ip_on_launch = true
+  enable_dns_hostnames     = true
+
+  tags = {
+    Name        = var.vpc_name
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+  public_subnet_tags = {
+    Name = "jenkins-subnet"
+  }
+}
+
+############################################
+# SECURITY GROUP FOR JENKINS + SONARQUBE
+############################################
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = var.jenkins_security_group
+  description = "Security Group for Jenkins + SonarQube"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "Jenkins"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 9000
+      to_port     = 9000
+      protocol    = "tcp"
+      description = "SonarQube"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "HTTPS"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+############################################
+# EC2 INSTANCE MODULE (UBUNTU 22.04 LTS)
+############################################
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = var.jenkins_ec2_instance
+
+  instance_type               = var.instance_type
+  ami                         = data.aws_ami.ubuntu.id   # <-- CHANGED FOR UBUNTU
+  key_name                    = "keypair"
+  monitoring                  = true
+  vpc_security_group_ids      = [module.sg.security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  availability_zone           = data.aws_availability_zones.azs.names[0]
+
+  # Ubuntu installation script (your updated install.sh)
+  user_data = file("../scripts/install_build_tools.sh")
+
+  tags = {
+    Name        = "Jenkins-Server-Ubuntu"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
