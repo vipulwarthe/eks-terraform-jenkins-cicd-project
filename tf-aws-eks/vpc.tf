9fc70f7f7620020a524f3d2cde388f7d25633717
Vipulkumar Warthe <126955342+vipulwarthe@users.noreply.github.com>
2025-11-14 16:33:35 +0530

    Create vpc.tf

---
+++ tf-aws-eks/vpc.tf
@@ -0,0 +1,37 @@
+############################################
+# VPC MODULE (AWS EKS Compatible VPC)
+############################################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.azs.names
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+  enable_vpn_gateway  = false
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = "1"
+  }
+}
